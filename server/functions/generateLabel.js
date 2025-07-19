51155a0d6d2e5b0ecb54a6075623134e4aa047b9
hamzaarif10 <legendhawk472@gmail.com>
2025-07-18 23:19:25 -0400

    Add generateLabel.js and functions folder

---
+++ server/functions/generateLabel.js
@@ -0,0 +1,34 @@
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  },
+});
+
+async function generatePdfLink(base64String, tracking) {
+  try {
+    const sanitizedBase64String = base64String.trim();
+    const buffer = Buffer.from(sanitizedBase64String, "base64");
+
+    const fileName = `labels/${tracking}.pdf`;
+
+    const uploadParams = {
+      Bucket: process.env.AWS_S3_BUCKET_NAME,
+      Key: fileName,
+      Body: buffer,
+      ContentType: "application/pdf"
+    };
+
+    await s3.send(new PutObjectCommand(uploadParams));
+
+    return `https://${process.env.AWS_S3_BUCKET_NAME}.s3.${process.env.AWS_REGION}.amazonaws.com/${fileName}`;
+  } catch (error) {
+    console.error("S3 upload failed:", error);
+    return null;
+  }
+}
+
+module.exports = { generatePdfLink };
